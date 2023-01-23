b7329c07529c778a618403230672d14efa209c58
Mary Williams <sbyardnss@gmail.com>
2023-01-23 14:30:07 -0600

    column 1 beginning  section 5

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -2,6 +2,7 @@
 import { DiamondSizes } from "./DiamondSizes.js"
 import { JewelryStyles } from "./JewelryStyles.js"
 import { Orders } from "./Orders.js"
+import { Metals } from "./Metals.js"
 
 document.addEventListener(
     "click",
@@ -16,12 +17,15 @@ export const KneelDiamonds = () => {
         <article class="choices">
             <section class="choices__metals options">
                 <h2>Metals</h2>
+                ${Metals()}
             </section>
             <section class="choices__sizes options">
                 <h2>Sizes</h2>
+                ${DiamondSizes()}
             </section>
             <section class="choices__styles options">
                 <h2>Styles</h2>
+                ${JewelryStyles()}
             </section>
         </article>
 
@@ -31,6 +35,7 @@ export const KneelDiamonds = () => {
 
         <article class="customOrders">
             <h2>Custom Jewelry Orders</h2>
+            ${Orders}
         </article>
     `
 }
